79c5ba0a8d99a52bf2f1bf24accfe72c7ab5bce9
petergabriel-dev <lopezpetergabriel@gmail.com>
2026-05-28 12:59:31 +0800

    Ignore tool dirs in Next dev watcher

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ["**/node_modules/**", "**/.git/**", "**/.pi/**", "**/.cocoindex_code/**"],
+    };
+
+    return config;
+  },
 };
 
 export default nextConfig;
